506dedb56f8676375c1d5d7fc24c4b4fb775ed1e
Hypronix <ytblockman@gmail.com>
2025-01-02 11:28:40 +0100

    Added rename function to r-click-menu

--- core/src/main/java/com/github/bitsky/StateMachineEditor.java
+++ core/src/main/java/com/github/bitsky/StateMachineEditor.java
@@ -16,6 +16,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class StateMachineEditor extends Editor{
     public final AnimationStateMachine stateMachine;
@@ -30,7 +31,30 @@ public class StateMachineEditor extends Editor{
         this.stateMachine = stateMachine;
         this.rightClickMenu = null;
 
-        actions.add(new ActionPair("Hello", () -> System.out.println("world")));
+        actions.add(new ActionPair("Create State", () -> {
+            Vector3 worldMouse3 = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+            Vector2 worldMouse = new Vector2(worldMouse3.x, worldMouse3.y);
+            stateMachine.addState(worldMouse.cpy());
+        }));
+
+        actions.add(new ActionPair("Rename", () -> {
+            StateDistancePair pair = getNearestStateToCursor();
+            if (pair.distance < 50) {
+                TextField input = new TextField(pair.state.name, ISpriteMain.getSkin());
+                Dialog dialog = new Dialog("Enter new name", ISpriteMain.getSkin(), "dialog") {
+                    public void result(Object obj) {
+                        if (obj instanceof String) {
+                            pair.state.name = input.getText();
+                        }
+                    }
+                };
+                dialog.setMovable(false);
+                dialog.button("Cancel");
+                dialog.button("Ok", "");
+                dialog.getContentTable().add(input);
+                dialog.show(stage);
+            }
+        }));
     }
 
     @Override
@@ -111,9 +135,7 @@ public class StateMachineEditor extends Editor{
             font.draw(spriteBatch, state.name, state.position.x-glyphLayout.width/2, state.position.y+glyphLayout.height/2);
         }
         spriteBatch.end();
-        if(Gdx.input.isKeyJustPressed(Input.Keys.G)){
-            stateMachine.addState(worldMouse.cpy());
-        }
+
         if(!Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
             moving = null;
         } else if(moving != null) {
@@ -123,30 +145,56 @@ public class StateMachineEditor extends Editor{
             connecting = null;
         }
 
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) && this.rightClickMenu != null) {
+            this.rightClickMenu.remove();
+            this.rightClickMenu = null;
+        }
+
         if (Gdx.input.isButtonJustPressed(Input.Buttons.RIGHT)) {
 
-            if (this.rightClickMenu != null)
+            if (this.rightClickMenu != null) {
                 this.rightClickMenu.remove();
+                this.rightClickMenu = null;
+            }
 
             this.rightClickMenu = new Table();
+
 
             for (ActionPair actionPair : actions) {
                 TextButton actionButton = new TextButton(actionPair.name, ISpriteMain.getSkin());
                 actionButton.addListener(new ClickListener() {
                     @Override
                     public void clicked(InputEvent event, float x, float y) {
-                        rightClickMenu.remove();
                         actionPair.getAction().run();
                         super.clicked(event, x, y);
                     }
                 });
-                this.rightClickMenu.add(actionButton);
+                this.rightClickMenu.add(actionButton).row();
             }
 
             rightClickMenu.setPosition(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
 
             this.stage.addActor(rightClickMenu);
         }
+    }
+
+    private StateDistancePair getNearestStateToCursor() {
+        Vector3 worldMouse3 = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+        Vector2 worldMouse = new Vector2(worldMouse3.x, worldMouse3.y);
+
+        ArrayList<AnimationStateMachine.State> sortedStates = new ArrayList<>(stateMachine.states.values());
+        sortedStates.sort((a, b) -> (int) (a.position.dst(worldMouse) - b.position.dst(worldMouse)));
+
+        final StateDistancePair pair = new StateDistancePair();
+        pair.state = sortedStates.get(0);
+        pair.distance = pair.state.position.dst(worldMouse);
+
+        return pair;
+    }
+
+    private static class StateDistancePair {
+        public float distance;
+        public AnimationStateMachine.State state;
     }
 
     private static class ActionPair {
